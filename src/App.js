44f4d5556c4c67571632ed6c7865ba81afbfd1f6
marcelacoronel <marcelacoronelp@gmail.com>
2021-02-10 22:27:58 -0500

    configuración archivos firebase y función obtener coleccion user

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ import './App.scss';
 import { Dashboard } from "./views/dashboardView/dashboard.js";
 // Importando componentes
 import Directory from "./views/Directory/Directory";
-import firebase from 'firebase/app'
+// import firebase from 'firebase/app'
 
-var firebaseConfig = {
-  apiKey: "AIzaSyB97MFDk0wRoFav5TnoI6OOYdsWYmt7ZIQ",
-  authDomain: "ecopetrol-tf.firebaseapp.com",
-  projectId: "ecopetrol-tf",
-  storageBucket: "ecopetrol-tf.appspot.com",
-  messagingSenderId: "488819244820",
-  appId: "1:488819244820:web:9e396db99bd0be01348c01"
-};
+// var firebaseConfig = {
+//   apiKey: "AIzaSyB97MFDk0wRoFav5TnoI6OOYdsWYmt7ZIQ",
+//   authDomain: "ecopetrol-tf.firebaseapp.com",
+//   projectId: "ecopetrol-tf",
+//   storageBucket: "ecopetrol-tf.appspot.com",
+//   messagingSenderId: "488819244820",
+//   appId: "1:488819244820:web:9e396db99bd0be01348c01"
+// };
 // Inicia Firebase
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
 
 
